d410f44034629f8e872c7b47808b20c8088a71dc
João Vitor Maia <maia.tostring@gmail.com>
2023-09-30 14:47:34 -0300

    Adding support for U-Type instructions on RISC-V decoder/encoder

--- src/steganossaurus/arch/riscv/isa/instructions/u_type.py
+++ src/steganossaurus/arch/riscv/isa/instructions/u_type.py
@@ -1,14 +1,57 @@
 from .instruction import Instruction, Field
+from typing import Literal
 
 class UType(Instruction):
 
     # CHECK
-    opcodes = ['1110011']
+    opcodes = [
+        '1110011',
+        '0010111',
+        '0110111',
+    ]
+    
+    instructions = {
+        '0010111': 'AIUPC',                
+        '0110111': 'LUI',                
+    }
 
     fields = [
+        Field('opcode', 0, 7),
         Field('rd', 7, 5),
         Field('imm', 12, 20),
     ]
 
     def __init__(self, source: str):
         super().__init__(source)
+        
+    def asm(self) -> str:
+        concat = self.get('imm')
+        
+        immediate = int(concat, 2), len(concat)
+        immediate = int(concat, 2)
+        
+        target_register = int(self.get('rd'), 2)
+        
+        try:
+            instruction = self.instructions[self.get('opcode')]
+        except KeyError:
+            raise ValueError("Unsupported instruction")
+
+        return f"{instruction} x{target_register}, x{immediate}"
+
+    def bin(self, endianess: Literal["little", "big"] = "little") -> bytes:
+        rep = f"{self.get('imm')}{self.get('rd')}{self.get('opciode')}"
+        value = int(rep, 2)
+        return value.to_bytes(4, endianess)
+
+    def mne(self):
+        try:
+            return self.instructions[self.get('opcode')]
+        except KeyError:
+            raise ValueError("Unsupported instruction")
+    
+    def __repr__(self):
+        return self.asm()
+
+    def __str__(self):
+        return self.asm()
